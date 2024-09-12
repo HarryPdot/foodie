7beeb02db6d613b6704320e7124877649d6e6e3a
HarryPdot <harrypham__@hotmail.com>
2024-09-12 17:50:21 +1000

    remove google route

--- app/api/Google/route.ts
+++
@@ -1,9 +0,0 @@
-
-const GET = async (query: string, type: string) => {
-    "use server";
-    const res = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&type=${type}&key=${process.env.API_KEY}`)
-    const data = await res.json()
-    return { data };
-}
-
-export { GET }
