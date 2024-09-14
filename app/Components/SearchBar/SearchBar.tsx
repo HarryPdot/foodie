4bfa024de81282d2cc533af419ece83234e7e279
HarryPdot <harrypham__@hotmail.com>
2024-09-14 15:30:24 +1000

    create nearbyplace and geocoding function

--- app/Components/SearchBar/SearchBar.tsx
+++ app/Components/SearchBar/SearchBar.tsx
@@ -9,19 +9,22 @@ const SearchBar = () => {
   const [address, setAddress] = useState("");
   const [data, setData] = useState([]);
   const searchApi = async (e: any) => {
+
     const value =
       search && address
-        ? `${search} food near ${address}`
-        : search || `food near ${address}`;
+        ? `${search} near ${address}`
+        : search
     try {
-      const url = new URL(
-        `/api/search/${value}?test=test`,
+      const url = !search && address ? new URL(
+        `/api/search/food?address=${address}`,
         window.location.origin,
-      );
+      )
+      : new URL(`/api/search/${value}?address=${address}`, window.location.origin);
       const res = await fetch(url.href, {
         method: "POST",
       });
       const data = await res.json();
+      console.log(data)
       setData(data);
     } catch {
       console.error("Internal Server Error:", Error);
@@ -53,10 +56,24 @@ const SearchBar = () => {
       {data.map((item: any, i: number) => {
         return (
           <div key={i} className={styles.card}>
-            <p>name: {item.name}</p>
-            <p>address: {item.address}</p>
-            <p>rating: {item.rating}</p>
-            <p>open now?:{item.open ? "true" : "false"}</p>
+            <div>
+              <p>name: {item.name}</p>
+              <p>address: {item.address}</p>
+              <p>rating: {item.rating}</p>
+              <p>open now?:{item.open ? "true" : "false"}</p>
+            </div>
+            { item.types? 
+              <p>types: 
+                {
+                item.types.map((type: string, i: number) => {
+                  return <p style={{border: "1px solid black"}} key={i}>{type}</p>;
+                })
+                }
+  
+              </p> 
+            : 
+              null
+            }
           </div>
         );
       })}
--- app/api/search/[value]/route.ts
+++ app/api/search/[value]/route.ts
@@ -6,15 +6,58 @@ interface Place {
   formatted_address: any;
   name: string;
   rating: number;
+  types: string[];
+}
+
+interface NearbySearch {
+  opening_hours: any;
+  vicinity: string;
+  name: string;
+  rating: number;
+  types: string[];
 }
 
 export async function POST(
   request: Request,
-  { params }: { params: { value: string } },
+  { params }: { params: { value: string }},
 ) {
+  const { value } = params;
+  const url = new URL(request.url);
+  const address = url.searchParams.get("address") || '';
+  console.log(address);
+  if(address && !value) {
+    console.log('1')
+  }
+
   try {
+    if(value === 'food') {
+      console.log('yes')
+      const geoRes = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address},nsw&key=${process.env.API_KEY}`,
+        {
+          method: 'GET'
+        }
+      )
+      const geoData = await geoRes.json();
+      const geoLocation = {
+        latitude: geoData.results[0].geometry.location.lat,
+        longitude: geoData.results[0].geometry.location.lng
+      }
+      const res = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${geoLocation.latitude},${geoLocation.longitude}&radius=1500&type=restaurant&keyword=${value}&key=${process.env.API_KEY}`)
+      const data = await res.json();
+      const newData = data.results.map((place: NearbySearch) => {
+        return {
+          name: place.name,
+          address: place.vicinity,
+          rating: place.rating,
+          open: place.opening_hours?.open_now,
+          types: place.types
+        };
+      });
+      return NextResponse.json(newData);
+    }
+
     const res = await fetch(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${params.value}&includedtype=restaurant&key=${process.env.API_KEY}&region=au`,
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${value}&includedtype=restaurant&key=${process.env.API_KEY}&region=au`,
     );
     const data = await res.json();
     const newData = data.results.map((place: Place) => {
@@ -23,9 +66,9 @@ export async function POST(
         address: place.formatted_address,
         rating: place.rating,
         open: place.opening_hours?.open_now,
+        types: place.types
       };
     });
-    console.log(params);
     return NextResponse.json(newData);
   } catch (error) {
     console.error(error);
