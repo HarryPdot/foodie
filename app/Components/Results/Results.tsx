74e55f0b51e1811226c5cfa9615b2630c48d90bd
HarryPdot <harrypham__@hotmail.com>
2024-09-16 19:20:10 +1000

    polish

--- app/Components/Results/Results.tsx
+++ app/Components/Results/Results.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Flex, Grid, Text, Badge } from "@radix-ui/themes";
+import { Badge, Box, Button, Card, Flex, Grid, Text } from "@radix-ui/themes";
 
 const Results = ({ data, dispatch }: { data: any; dispatch: any }) => {
   return (
@@ -7,11 +7,24 @@ const Results = ({ data, dispatch }: { data: any; dispatch: any }) => {
         return (
           <Card key={i} size="2" variant="classic">
             <Flex direction={"column"}>
-              <Flex direction={"column"} gap={'1'}>
+              <Flex direction={"column"} gap={"1"}>
                 <Text>{item.name}</Text>
                 <Text>{item.address}</Text>
                 <Text>Rating: {item.rating}</Text>
-                {item.open ? <Badge color="green" variant="soft" size={'2'} radius="medium">Open</Badge> : <Badge color="red" size={'2'} radius="medium">Closed</Badge>}
+                {item.open ? (
+                  <Badge
+                    color="green"
+                    variant="soft"
+                    size={"2"}
+                    radius="medium"
+                  >
+                    Open
+                  </Badge>
+                ) : (
+                  <Badge color="red" size={"2"} radius="medium">
+                    Closed
+                  </Badge>
+                )}
               </Flex>
             </Flex>
           </Card>
@@ -22,28 +35,3 @@ const Results = ({ data, dispatch }: { data: any; dispatch: any }) => {
 };
 
 export { Results };
-
-// {data.map((item: any, i: number) => {
-//   return (
-//     <div key={i} className={styles.card}>
-//       <Text>
-//         <p>name: {item.name}</p>
-//         <p>address: {item.address}</p>
-//         <p>rating: {item.rating}</p>
-//         <p>open now?:{item.open ? "true" : "false"}</p>
-//       </Text>
-//       {item.types ? (
-//         <p>
-//           types:
-//           {item.types.map((type: string, i: number) => {
-//             return (
-//               <p style={{ border: "1px solid black" }} key={i}>
-//                 {type}
-//               </p>
-//             );
-//           })}
-//         </p>
-//       ) : null}
-//     </div>
-//   );
-// })}
