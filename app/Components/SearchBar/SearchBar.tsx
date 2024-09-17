0ae5e4a0306b6caa3b8f96c198127117cca7b6da
HarryPdot <harrypham__@hotmail.com>
2024-09-17 17:22:03 +1000

    install radix colors, rebuild form

--- app/Components/SearchBar/SearchBar.tsx
+++ app/Components/SearchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button, Flex, Section, TextField } from "@radix-ui/themes";
+import * as Form from "@radix-ui/react-form";
+import styles from './card.module.css'
 
 import { fetcher } from "../../Service/fetch";
 
@@ -44,22 +46,54 @@ const SearchBar = ({ state, dispatch }: { state: any; dispatch: any }) => {
   };
   
   return (
-    <Section py={'2'}>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <Flex gap={'3'} direction={'column'}>
-          <TextField.Root placeholder="Search" onChange={(e) => handleSearchInput(e)}>
-            <TextField.Slot>
-            </TextField.Slot>
-          </TextField.Root>
-          <TextField.Root placeholder="Address" onChange={(e) => handleAddressInput(e)}>
-            <TextField.Slot>
-            </TextField.Slot>
-          </TextField.Root>
-          <Button disabled={search && !address || !search && !address} onClick={() => searchApi()}>Search</Button>
-        </Flex>
-      </form>
-    </Section>
+    <Form.Root className='FormRoot' onSubmit={(event) => event.preventDefault()}>
+      <Flex direction={'column'} gap={'1'}>
+        <Form.Field className="FormField" name='search'>
+          <Flex align={'baseline'} justify={'between'}>
+            <Form.Label className="FormLabel"> Search </Form.Label>
+          </Flex>
+            <Form.Control asChild>
+              <TextField.Root className="Input" onChange={(e) => handleSearchInput(e)} required>
+                <TextField.Slot>
+                </TextField.Slot>
+              </TextField.Root>
+            </Form.Control>
+
+        </Form.Field>
+        <Form.Field className="FormField" name='address'>
+          <Flex align={'baseline'} justify={'between'}>
+            <Form.Label className="FormLabel">Address</Form.Label>
+            <Form.Message className="FormMessage" match={'valueMissing'}>Place enter an address</Form.Message>
+          </Flex>
+            <Form.Control asChild>
+              <TextField.Root className="Input" onChange={(e) => handleAddressInput(e)} required>
+                <TextField.Slot>
+                </TextField.Slot>
+              </TextField.Root>
+            </Form.Control>
+        </Form.Field>
+        <Form.Submit asChild>
+          <Button className="Button" disabled={search && !address || !search && !address} onClick={() => searchApi()}>Search</Button>
+        </Form.Submit>
+      </Flex>
+    </Form.Root>
   );
 };
 
 export { SearchBar };
+
+// <Section py={'2'}>
+// <form onSubmit={(event) => event.preventDefault()}>
+//   <Flex gap={'3'} direction={'column'}>
+//       <TextField.Root placeholder="Search" onChange={(e) => handleSearchInput(e)} required>
+//         <TextField.Slot>
+//         </TextField.Slot>
+//       </TextField.Root>
+//     <TextField.Root placeholder="Address" onChange={(e) => handleAddressInput(e)}>
+//       <TextField.Slot>
+//       </TextField.Slot>
+//     </TextField.Root>
+//     <Button disabled={search && !address || !search && !address} onClick={() => searchApi()}>Search</Button>
+//   </Flex>
+// </form>
+// </Section>
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
           grayColor="gray"
           radius="medium"
         >
-          {/* <AuthButtonServer /> */}
           {children}
         </Theme>
       </body>
