934a4535a2eff2985a7d4ee3e46716248e3bf3b7
HarryPdot <harrypham__@hotmail.com>
2024-09-22 19:52:16 +1000

    Overwrite address component

--- app/Components/NavBar/Address/Address.tsx
+++ app/Components/NavBar/Address/Address.tsx
@@ -1,73 +1,42 @@
 "use client";
-import * as Dialog from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
-import * as Label from "@radix-ui/react-label";
-import { clsx } from "clsx";
-import React from "react";
-
-import styles from "./Address.module.css";
+import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import styles from "./Address.module.css"
+import { clsx } from 'clsx'
+import * as Label from '@radix-ui/react-label';
 const Address = () => {
-  return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
-        <button
-          className={clsx({ [styles.Button]: true, [styles.violet]: true })}
-        >
-          Address
-        </button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className={styles.DialogOverlay} />
-        <Dialog.Content className={styles.DialogContent}>
-          <Dialog.Title className={styles.DialogTitle}>Address</Dialog.Title>
-          <div
-            style={{
-              display: "flex",
-              padding: "0 20px",
-              flexWrap: "wrap",
-              gap: 15,
-              alignItems: "center",
-            }}
-          >
-            <Label.Root
-              className={styles.LabelRoot}
-              htmlFor="firstName"
-            ></Label.Root>
-            <input
-              className={styles.Input}
-              type="text"
-              id="firstName"
-              placeholder="Search an Address"
-            />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              marginTop: 25,
-              justifyContent: "flex-end",
-            }}
-          >
-            <Dialog.Close asChild>
-              <button
-                className={clsx({
-                  [styles.Button]: true,
-                  [styles.green]: true,
-                })}
-              >
-                Save changes
-              </button>
-            </Dialog.Close>
-          </div>
+    return (
+        <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <button className={clsx({[styles.Button]:true, [styles.violet]:true})}>Address</button>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className={styles.DialogOverlay} />
+              <Dialog.Content className={styles.DialogContent}>
+                <Dialog.Title className={styles.DialogTitle}>Address</Dialog.Title>
+                <div style={{ display: 'flex', padding: '0 20px', flexWrap: 'wrap', gap: 15, alignItems: 'center' }}
+                    >
+                    <Label.Root className={styles.LabelRoot} htmlFor="firstName">
 
-          <Dialog.Close asChild>
-            <button className={styles.IconButton} aria-label="Close">
-              <Cross2Icon />
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
-};
+                    </Label.Root>
+                    <input className={styles.Input} type="text" id="firstName" placeholder='Search an Address' />
+                </div>
+                <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
+                  <Dialog.Close asChild>
+                    <button className={clsx({[styles.Button]:true, [styles.green]:true})}>Save changes</button>
+                  </Dialog.Close>
+                </div>
 
-export { Address };
+                <Dialog.Close asChild>
+                  <button className={styles.IconButton} aria-label="Close">
+                    <Cross2Icon />
+                  </button>
+                </Dialog.Close>
+              </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
+
+export { Address }
